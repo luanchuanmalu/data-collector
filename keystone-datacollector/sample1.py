2956bcf965973fff8e0df155b6d70b25d25f8b37
luanchuanmalu <jian_hua_yang@163.com>
2015-09-21 17:43:41 +0800

    init

---
+++ keystone-datacollector/sample1.py
@@ -0,0 +1,12 @@
+class MyStuff(object):
+    def __init__(self):
+        self.tangerline = "And now a thousand years between"
+
+    def apple(self):
+        print "I AM CLASSY APPLES!"
+
+
+if __name__ == "__main__":
+    thing = MyStuff()
+    thing.apple()
+    print thing.tangerline
